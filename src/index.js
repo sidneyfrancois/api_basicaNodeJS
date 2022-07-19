359e3b11a5f35fd8198b5579836a7ee2afbf43e4
Sidney François <sidney.fvm.ps@outlook.com>
2022-07-18 22:03:58 -0300

    definindo primeira rota e package.json

--- src/index.js
+++ src/index.js
@@ -2,4 +2,15 @@ const express = require("express");
 
 const app = express();
 
+/**
+ * cpf       - string
+ * name      - string
+ * id        - uuid
+ * statement -  []
+ */
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+});
+
 app.listen(3333);
