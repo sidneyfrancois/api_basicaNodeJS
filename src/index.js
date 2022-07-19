06a0d451026f38c4a5fb53e7a2acf7d56743d586
Sidney François <sidney.fvm.ps@outlook.com>
2022-07-19 11:32:57 -0300

    adicionando middlewares nas funções de coleta de dados bancários

--- src/index.js
+++ src/index.js
@@ -14,6 +14,38 @@ const customers = [];
  * statement -  []
  */
 
+/**
+ * Funções de Middleware
+ */
+
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
+function verifyIfExistsAccountCPFParam(request, response, next) {
+  const { cpf } = request.params;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -35,27 +67,17 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement/:cpf", (request, response) => {
-  const { cpf } = request.params;
-
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: "Customer not found!" });
+app.get(
+  "/statement/:cpf",
+  verifyIfExistsAccountCPFParam,
+  (request, response) => {
+    const { customer } = request;
+    return response.json(customer.statement);
   }
+);
 
-  return response.json(customer.statement);
-});
-
-app.get("/statementHeader", (request, response) => {
-  const { cpf } = request.headers;
-
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: "Customer not found!" });
-  }
-
+app.get("/statementHeader", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
   return response.json(customer.statement);
 });
 
