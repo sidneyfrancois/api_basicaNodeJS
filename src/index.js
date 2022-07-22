22df0844382779359e4f5c5ed1f24d20f40e05c9
Sidney François <sidney.fvm.ps@outlook.com>
2022-07-22 14:47:37 -0300

    adicionando rota para retornar dados da conta

--- src/index.js
+++ src/index.js
@@ -154,4 +154,10 @@ app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
   return response.status(201).send();
 });
 
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+});
+
 app.listen(3333);
